f61d6dca210a3d85c0757ec26c48d76b3bbc7d48
Anja Stanojevic <anja.stanojevic@gmail.com>
2019-10-07 10:24:36 +0200

    Use only one instance of Jersey client for all calls

--- client/src/main/java/com/anjastanojevic/client/BookClient.java
+++ client/src/main/java/com/anjastanojevic/client/BookClient.java
@@ -1,16 +1,21 @@
 package com.anjastanojevic.client;
 
-import com.anjastanojevic.client.Book;
-
-import javax.ws.rs.client.*;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
 
 public class BookClient {
 
+    private Client client;
+
+    public BookClient() {
+        client = ClientBuilder.newClient();
+    }
+
     public Book getBookByIsbn(String isbn){
 
-        Book book = ClientBuilder
-                .newClient()
+        Book book = client
                 .target("http://localhost:8080/book?isbn=" + isbn)
                 .request(MediaType.APPLICATION_JSON)
                 .get(Book.class);
@@ -18,12 +23,11 @@ public class BookClient {
         return book;
     }
 
-    public void addBook(Book book){
+    public void addBook(Book book) {
 
-        ClientBuilder
-                .newClient()
-                .target("http://localhost:8080/book")
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.entity(book, MediaType.APPLICATION_JSON));
+        client
+            .target("http://localhost:8080/book")
+            .request(MediaType.APPLICATION_JSON)
+            .post(Entity.entity(book, MediaType.APPLICATION_JSON));
     }
 }
